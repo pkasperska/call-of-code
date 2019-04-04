c9c1855b2d2873fb3378f13e386abab48759db01
pkasperska <paulina.rychlowska@gmail.com>
2019-04-04 20:35:14 +0200

    User's GPS location & counter distance to nearest city

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const api = express();
+const cities = require('./cities.json')
+
+const toRadians = num => num * Math.PI / 180;
+
+function getNearestCity(currentPosition) {
+  for (city of cities) {
+    const cityName = city.name;
+    const distanceInKm = countDistance(city, currentPosition)
+   
+    if (distanceInKm < 10) {
+      return cityName;
+    }
+  }
+}
+function countDistance(city, currentPosition) {
+  const earthRadiusInKm = 6371;
+  const {latitude, longitude} = currentPosition;
+  const cityLat = parseFloat(city.cityLat);
+  const cityLng = parseFloat(city.cityLng);
+  const deltaLatitude = toRadians(latitude - cityLat);
+  const deltaLongitude = toRadians(longitude - cityLng);
+
+  const a = Math.sin(deltaLatitude / 2) * Math.sin(deltaLatitude / 2) +
+    Math.sin(deltaLongitude / 2) * Math.sin(deltaLongitude / 2) *
+    Math.cos(cityLat) * Math.cos(latitude);
+  const c = 2 * Math.atan(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return c * earthRadiusInKm;
+}
+
+
+
+api.listen("3200", () => {
+    console.log("Started on port 3200");
+  });
+  
+api.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+  
+api.get("/city", (request, response) => {
+    const nearestCity = getNearestCity(request.query);
+    response.send({nearestCity});
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gpsPoints: [],
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    this.getGps().then(({ coords }) => {
+      const { latitude, longitude } = coords;
+      this.setState({ gpsPoints: [longitude, latitude] });
+      fetch(`http://localhost:3200/city?longitude=${longitude}&latitude=${latitude}`)
+      .then(response => response.json())
+      .then(({nearestCity}) => {
+        this.setState({data: nearestCity})
+      })
+    });
+  }
+
+  getGps() {
+    return new Promise((resolve, reject) => {
+      window.navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+  }
+
+  renderGpsLocations() {
+    const [latitude, longitude] = this.state.gpsPoints;
+    console.log(this.state);
+    return <span>{`${latitude}, ${longitude}`}</span>;
+  }
+
   render() {
     return (
-      <div className="App">
+      <Fragment>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <Fragment>Twoja pozycja to: {this.renderGpsLocations()} Najbliższe miasto to: {this.state.data}</Fragment>
         </header>
-      </div>
+      </Fragment>
     );
   }
 }
