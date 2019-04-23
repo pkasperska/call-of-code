4bf4d1a49ef68f2b9c2a7399df8111e44e74a869
pkasperska <paulina.rychlowska@gmail.com>
2019-04-23 18:42:34 +0200

    Updated function which count distance

--- backend/server.js
+++ backend/server.js
@@ -1,49 +1,55 @@
-const express = require('express');
+const express = require("express");
 const api = express();
-const cities = require('./cities.json')
+const cities = require("./cities.json");
 
-const toRadians = num => num * Math.PI / 180;
+const toRadians = num => (num * Math.PI) / 180;
 
 function pickCityForLocation(currentPosition, cities) {
-  const calculatedCitiesWithDistances = cities.map(city => ({city, distance: countDistance(city, currentPosition)}));
-  const nearCity = calculatedCitiesWithDistances.reduce((nearestCityWithDistance, cityWithDistance) => {
-    return nearestCityWithDistance.distance < cityWithDistance.distance ? 
-      nearestCityWithDistance : cityWithDistance
-  });
-  console.log(calculatedCitiesWithDistances)
+  const calculatedCitiesWithDistances = cities.map(city => ({
+    city,
+    distance: countDistance(city, currentPosition)
+  }));
+  const nearCity = calculatedCitiesWithDistances.reduce(
+    (nearestCityWithDistance, cityWithDistance) => {
+      return nearestCityWithDistance.distance < cityWithDistance.distance
+        ? nearestCityWithDistance
+        : cityWithDistance;
+    }
+  );
   return nearCity.city.name;
 }
 
 function countDistance(city, currentPosition) {
   const earthRadiusInKm = 6371;
-  const {latitude, longitude} = currentPosition;
-  const cityLat = parseFloat(city.cityLat);
-  const cityLng = parseFloat(city.cityLng);
-  const deltaLatitude = toRadians(latitude - cityLat);
-  const deltaLongitude = toRadians(longitude - cityLng);
+  const { latitude, longitude } = currentPosition;
+  const cityLat = toRadians(parseFloat(city.cityLat));
+  const cityLng = toRadians(parseFloat(city.cityLng));
+  const latitudeRadians = toRadians(latitude);
+  const longitudeRadians = toRadians(longitude)
+  const delta = longitudeRadians - cityLng;
 
-  const a = Math.sin(deltaLatitude / 2) * Math.sin(deltaLatitude / 2) +
-    Math.sin(deltaLongitude / 2) * Math.sin(deltaLongitude / 2) *
-    Math.cos(cityLat) * Math.cos(latitude);
-  const c = 2 * Math.atan(Math.sqrt(a), Math.sqrt(1 - a));
-
-  return c * earthRadiusInKm;
+  const distance =
+    Math.acos(
+      Math.sin(cityLat) * Math.sin(latitudeRadians) +
+        Math.cos(cityLat) * Math.cos(latitudeRadians) * Math.cos(delta)
+    ) * earthRadiusInKm;
+  return distance;
 }
 
-
-
 api.listen("3200", () => {
-    console.log("Started on port 3200");
-  });
-  
-api.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  
-  
+  console.log("Started on port 3200");
+});
+
+api.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 api.get("/city", (request, response) => {
-    const nearestCity = pickCityForLocation(request.query, cities);
-    response.send({nearestCity});
+  const nearestCity = pickCityForLocation(request.query, cities);
+  response.send({ nearestCity });
 });
